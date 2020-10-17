b77f1a55c2495d8924cd73c27810a6b46c2d37b9
jstN0body <raoboys12@gmail.com>
2020-10-17 14:58:49 -0400

    Update Raid Event

--- src/main/java/com/github/jstN0body/twitchintegration/twitchbot/events/TwitchRaidEvent.java
+++ src/main/java/com/github/jstN0body/twitchintegration/twitchbot/events/TwitchRaidEvent.java
@@ -3,6 +3,7 @@ package com.github.jstN0body.twitchintegration.twitchbot.events;
 import com.github.philippheuer.events4j.simple.SimpleEventHandler;
 import com.github.twitch4j.chat.events.channel.RaidEvent;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
@@ -14,12 +15,11 @@ public class TwitchRaidEvent {
     }
 
     public void onRaid(RaidEvent event) {
-        Bukkit.dispatchCommand(Bukkit.getConsoleSender(),
-                "title @a title {\"text\":\"" + event.getRaider().getName() + " has raided!!!\",\"bold\":true, \"color\":\"blue\"} times 10 80 10");
+        Bukkit.broadcastMessage(ChatColor.BLUE + event.getRaider().getName() + " has raided with " + event.getViewers() + " viewers!!!");
         Object[] players = Bukkit.getOnlinePlayers().toArray();
         for (Object object : players) {
             Player player = (Player) object;
-            player.addPotionEffect(new PotionEffect(PotionEffectType.BAD_OMEN, 36000, Math.round(event.getViewers() / 3)));
+            player.addPotionEffect(new PotionEffect(PotionEffectType.BAD_OMEN, 36000, Math.round(event.getViewers() / 3f)));
         }
     }
 }
